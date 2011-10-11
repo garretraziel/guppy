bd706e0543fc2e8917cacf3a34acf1d4db172dd1
alda519 <galda519@gmail.com>
2011-10-11 16:16:18 +0200

    zpracovani parametru v main

---
+++ src/ifj11.h
@@ -0,0 +1,18 @@
+/*
+ * projekt: interpret jazyka IFJ11 
+ * soubor: ifj11.h
+ * autori:
+ *   xbrabe09 Brabec Lukas
+ *   xdujic01 Dujicek Ales
+ *   xmokra00 Mokra Radka
+ *   xsedla85 Sedlak Jan
+ */
+
+
+#ifndef IFJ_IFJ11_H
+#define IFJ_IFJ11_H
+
+/// Navratove kody programu
+enum {ERROR_LEX = 1, ERROR_SYN, ERROR_SEM, ERROR_INT, ERROR_FAIL} EErrors;
+
+#endif
--- src/main.c
+++ src/main.c
@@ -1,7 +1,52 @@
-#include <stdio.h>
+/*
+ * projekt: interpret jazyka IFJ11 
+ * soubor: main.c
+ * autori:
+ *   xbrabe09 Brabec Lukas
+ *   xdujic01 Dujicek Ales
+ *   xmokra00 Mokra Radka
+ *   xsedla85 Sedlak Jan
+ */
 
-int main(void)
+#include <stdio.h>
+#include <stdarg.h>
+
+#include "ifj11.h"
+
+
+/// Zprava napovedy.
+const char HELP_MSG[] =
+    "ifj11 - interpret jazyka ifj11\n"
+    "pouziti:\n"
+    "    ifj11 SOUBOR \n";
+
+
+/// Funkce vypisuje chybova hlaseni
+void Error(const char *fmt, ...)
 {
+    va_list args;
+    va_start(args, fmt);
+    fprintf(stderr, "CHYBA: ");
+    vfprintf(stderr, fmt, args);
+    va_end(args);
+}
+
+
+/// Funkce main
+int main(int argc, char *argv[])
+{
+    if(argc != 2) {
+        Error("Chybny pocet parametru.\n\n");
+        fprintf(stderr, HELP_MSG);
+        return ERROR_FAIL;
+    }
+
+    FILE *file = fopen(argv[1], "r");
+    if(file == NULL) {
+        Error("Nepodarilo se otevrit soubor %s\n)", argv[1]);
+        return ERROR_FAIL;
+    }
+    fclose(file);
 
     return 0;
 }

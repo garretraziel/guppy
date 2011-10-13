7a5c64140d1b3ad065cb3b68e2b10ab026a55cf1
alda519 <galda519@gmail.com>
2011-10-13 22:26:46 +0200

    dokoncen prototyp konecneho automatu

--- src/lexical.c
+++ src/lexical.c
@@ -20,13 +20,16 @@
 
 /// Stavy konecneho automatu
 enum {
-    FSM_ALPHA,
     FSM_COMMA,
     FSM_DASH,
     FSM_DIV,
     FSM_DOT,
     FSM_EQUALS,
+    FSM_ESCAPE,
+    FSM_ESCAPE_NUM,
+    FSM_ESCAPE_NUM2,
     FSM_EXP,
+    FSM_EXP_E,
     FSM_FLOAT,
     FSM_GREAT,
     FSM_IDENTIFIER,
@@ -55,6 +58,8 @@ int get_token(FILE *input, string *value)
     static int c;
     static int state = FSM_READ;
 
+    int num;
+
     while(state != FSM_TERMINATE) {
 
         switch(state) {
@@ -66,7 +71,7 @@ int get_token(FILE *input, string *value)
                 if(isspace(c))
                     state = FSM_READ;
                 else if(isalpha(c) || c == '_')
-                    state = FSM_ALPHA;
+                    state = FSM_IDENTIFIER;
                 else if(isdigit(c))
                     state = FSM_NUMBER;
                 else if(c == '+')
@@ -105,24 +110,17 @@ int get_token(FILE *input, string *value)
                     return ERROR;
                 break;
 
-            case FSM_ALPHA: // imho ok
+            case FSM_IDENTIFIER:
                 str_push(value, c);
-                state = FSM_IDENTIFIER;
-                break;
-
-
-            case FSM_IDENTIFIER: // imho ok
                 c = fgetc(input);
-                if(isalnum(c) || c == '_')
-                    str_push(value, c);
-                else {
+                if(! (isalnum(c) || c == '_')) {
                     state = FSM_START;
                     // TODO kontrola klicovych slov
                     return IDENTIFIER;
                 }
                 break;
 
-            case FSM_NUMBER: // imho OK
+            case FSM_NUMBER:
                 str_push(value, c);
                 c = fgetc(input);
                 if(isdigit(c))
@@ -137,40 +135,180 @@ int get_token(FILE *input, string *value)
                 }
                 break;
 
-            case FSM_FLOAT: // imho OK
+            case FSM_FLOAT:
                 str_push(value, c);
                 c = fgetc(input);
                 if(isdigit(c))
                     ;
                 else if(c == 'E' || c == 'e')
-                    state = FSM_EXP;
+                    state = FSM_EXP_E;
                 else {
                     state = FSM_START;
                     return NUMBER;
                 }
                 break;
 
-            case FSM_EXP:
-                if(c == '+' || c == '-')
-                    ;
-                break;
-
-            case FSM_SIGNED_EXP:
-                break;
-
-            case FSM_OPERATOR: // moc obecne
+            case FSM_EXP_E:
                 str_push(value, c);
-                state = FSM_READ;
-                return OPERATOR;
-                break;
-            
-            case FSM_STRING:
-                return STRING;
+                c = fgetc(input);
+                if(c == '+' || c == '-')
+                    state = FSM_EXP;
+                else if(isdigit(c))
+                    state = FSM_EXP;
+                else { 
+                    state = FSM_START;
+                    return ERROR;
+                }
                 break;
 
-            case FSM_SEMICOLON: // imho ok
+            case FSM_EXP:
+                str_push(value, c);
+                c = fgetc(input);
+                if(! isdigit(c)) {
+                    state = FSM_START;
+                    return NUMBER;
+                }
+                break;
+
+            case FSM_STRING:
+                c = fgetc(input);
+                if(c < 1) {
+                    state = FSM_STRING; // TODO: zvazit, kam to ma pokracovat
+                    return ERROR;
+                } else if(c == '\'')
+                    state = FSM_ESCAPE;
+                else if(c == '"') {
+                    return STRING;
+                } else
+                    str_push(value, c);
+                break;
+
+            case FSM_ESCAPE:
+                c = fgetc(input);
+                if(c == 'n' || c == 't' || c == '\\' || c == '"') {
+                    state = FSM_STRING;
+                    str_push(value, c);
+                } else if(isdigit(c))
+                    state = FSM_ESCAPE_NUM;
+                else {
+                    state = FSM_STRING; // TODO: zvazit, kam to ma pokracovat
+                    return ERROR;
+                }
+                break;
+
+// TODO: tu ciselnou escape sekvenci vymyslet lepe
+
+            case FSM_ESCAPE_NUM:
+                num = c - '0';
+                c = fgetc(input);
+                if(isdigit(c)) { 
+                    num = num * 10 + c - '0';
+                    state = FSM_ESCAPE_NUM2;
+                }
+                else {
+                    state = FSM_STRING; // TODO: zvazit, kam to ma pokracovat
+                    return ERROR;
+                }
+                break;
+
+            case FSM_ESCAPE_NUM2: // TODO
+                c = fgetc(input);
+                if(isdigit(c)) { 
+                    num = num * 10 + c - '0';
+                    if(num > 0 && num < 256) {
+                        str_push(value, num);
+                        state = FSM_STRING;
+                    }
+                }
+                //push?
+                state = FSM_STRING; // TODO: zvazit, kam to ma pokracovat
+                return ERROR;
+                break;
+
+            case FSM_DOT:
+                c = fgetc(input);
+                if(c == '.') {
+                    state = FSM_READ;
+                    return STRCONCAT;
+                } else {
+                    state = FSM_START;
+                    return ERROR;
+                }
+                break;
+
+            case FSM_LESS:
+                c = fgetc(input);
+                if(c == '=') {
+                    state = FSM_READ;
+                    return LESS_EQ;
+                } else {
+                    state = FSM_START;
+                    return LESS;
+                }
+                break;
+
+            case FSM_GREAT:
+                c = fgetc(input);
+                if(c == '=') {
+                    state = FSM_READ;
+                    return GREAT_EQ;
+                } else {
+                    state = FSM_START;
+                    return GREAT;
+                }
+                break;
+
+            case FSM_TILDA:
+                c = fgetc(input);
+                if(c == '=') {
+                    state = FSM_READ;
+                    return TILDA;
+                } else {
+                    state = FSM_START;
+                    return ERROR;
+                }
+                break;
+
+// TODO: zkusit nejak smrsknout kod nasledujicich stavu:
+
+            case FSM_SEMICOLON:
                 state = FSM_READ;
                 return SEMICOLON;
+                break;
+
+            case FSM_PLUS:
+                state = FSM_READ;
+                return PLUS;
+                break;
+
+            case FSM_DIV:
+                state = FSM_READ;
+                return DIV;
+                break;
+
+            case FSM_MUL:
+                state = FSM_READ;
+                return MUL;
+                break;
+
+            case FSM_POWER:
+                state = FSM_READ;
+                return POWER;
+                break;
+
+            case FSM_LBRAC:
+                state = FSM_READ;
+                return LBRAC;
+                break;
+
+            case FSM_RBRAC:
+                state = FSM_READ;
+                return RBRAC;
+                break;
+
+            case FSM_COMMA:
+                state = FSM_READ;
+                return COMMA;
                 break;
 
             case FSM_TERMINATE:
@@ -178,7 +316,7 @@ int get_token(FILE *input, string *value)
                 break;
 
             default:
-                printf("Not yet implemented! \n");
+                printf("Not yet implemented! [%c]\n", state);
                 state = FSM_READ;
                 break;
         }

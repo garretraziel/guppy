e0596040fa0abd84a2f540f9dd2a6dc9961db154
alda519 <galda519@gmail.com>
2011-11-12 20:18:10 +0100

    pridana hlaseni syntaktickych chyb

--- src/parser.h
+++ src/parser.h
@@ -11,6 +11,25 @@
 #ifndef IFJ_PARSER_H
 #define IFJ_PARSER_H
 
+enum {
+    ERROR_SYN_UX_TOKEN = -1,
+    ERROR_SYN_X_SMCLN = -2,
+    ERROR_SYN_X_EOF = -3,
+    ERROR_SYN_X_FUNC = -4,
+    ERROR_SYN_X_IDENT = -5,
+    ERROR_SYN_X_END = -6,
+    ERROR_SYN_X_ASGN = -7,
+    ERROR_SYN_X_LBRC = -8,
+    ERROR_SYN_X_RBRC = -9,
+    ERROR_SYN_X_THEN = -10,
+    ERROR_SYN_X_ELSE = -11,
+    ERROR_SYN_X_DO = -12,
+    ERROR_SYN_X_OPRTR = -13,
+} ESynErrors;
+
+// Zpracuje pocatacni neterminalni symbol
 int program(FILE *);
+
+extern const char *SYN_ERRORS[];
 
 #endif

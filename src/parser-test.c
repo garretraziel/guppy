30d50aa8cc5713e636b7699ac644e869de02ad69
alda519 <galda519@gmail.com>
2011-11-13 12:45:00 +0100

    zapomnel jsem pridat test parseru

---
+++ src/parser-test.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+#include "parser.h"
+#include "lexical.h"
+
+void Error(const char *fmt, ...)
+{
+    va_list args;
+    va_start(args, fmt);
+    fprintf(stderr, "CHYBA: ");
+    vfprintf(stderr, fmt, args);
+    va_end(args);
+}
+
+int main(void)
+{
+
+    int x = program(stdin);
+    
+    // predpoklada se, ze chyby jsou jen syntakticke
+    // lexikalni se testuji jinde
+    // semanticke jeste testovat neumim
+    if(x < 0)
+        Error("Na radku %d: %s\n", line, SYN_ERRORS[-x]);
+    
+    // casem se napise nejake to udelatko na vsechny chyby
+
+    return 0;
+}

91eb72a846938400dbb4f85ad38e11c5220b260a
Garret Raziel <boloomka@gmail.com>
2011-11-30 21:17:45 +0100

    nova vetev pro interpret

---
+++ src/exec.c
@@ -0,0 +1,14 @@
+/*
+ * projekt: interpret jazyka IFJ11 
+ * soubor: exec.c
+ * autori:
+ *   xbrabe09 Brabec Lukas
+ *   xdujic01 Dujicek Ales
+ *   xmokra00 Mokra Radka
+ *   xsedla85 Sedlak Jan
+ */
+
+#include <stdlib.h>
+
+#include "exec.h"
+#include "string.h"

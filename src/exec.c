341e05eb8e731d665b550bb5e5a37f9bd915521b
Garret Raziel <boloomka@gmail.com>
2011-11-30 23:29:28 +0100

    pridan mustr pro generate

--- src/exec.c
+++ src/exec.c
@@ -52,6 +52,16 @@ PTapeItem actnext_tape() /// posune aktivni prvek na dalsi a vrati ho
 
 int generate(int instr, void *adr, int type) /// funkce, ktera prebere typ instrukce, ukazatel a jeho typ a vygeneruje podle toho instrukci
 {
+    //TODO: takto jednoduse to samozrejme nejde, ale aspon aby byl mustr. joa funkce by mohla vracet adresu te vygenerovane instrukce
+    //TODO: jinak se asi bude muset switchovat podle typu instrukce a vytvaret vsechny ty dodatecny veci, co jsou k tomu potreba
+    PTapeItem item = malloc(sizeof(struct TTapeItem));
+    if (item == NULL) return -1; //TODO: potreba udelat errory pro nedostatek pameti
+    item -> instr = instr;
+    item -> adr = adr;
+    item -> adrtype = type;
+    item -> next = NULL;
+    if (add_to_tape(item) != 0) return -1;
+    
     return 0;
 }
 

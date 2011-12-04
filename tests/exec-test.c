6495a7d7b0987db1d3038b6a9160585f792f1464
Garret Raziel <boloomka@gmail.com>
2011-12-04 13:28:38 +0100

    v testech k exec.c testuju vypisovani cisla a stringu

--- tests/exec-test.c
+++ tests/exec-test.c
@@ -1,18 +1,36 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #include "exec.h"
 #include "defines.h"
 #include "ial.h"
 
+char *strcpy(char *, char*);
+
 int main(void) {
+    
     init_tape();
-    Data name;
-    name.type = T_NUMBER;
-    name.value.num = 1;
-    insert_literal(name);
+    
+    Data str;
+    str.type = T_STRING;
+    str.value.str = malloc(7);
+    strcpy(str.value.str,"\nahoj\n");
+    insert_literal(str);
+    
     generate(IPUSH, last_literal, ALITTABLE);
+    
+    Data number;
+    number.type = T_NUMBER;
+    number.value.num = 1;
+    insert_literal(number);
+    
+    generate(IPUSH, last_literal, ALITTABLE);
+    
+    generate(IWRITE, NULL, ANONE);
     generate(IWRITE, NULL, ANONE);
     generate(IHALT, NULL, ANONE);
+    
     execute();
     drop_literals();
 }

538b49ed767665e2ea07273f1e4abd6730eb3ed1
Lukas Sparrow <lukas.sparrow@gmail.com>
2011-12-06 17:33:26 +0100

    prace na CMPL

--- src/exec.c
+++ src/exec.c
@@ -379,8 +379,34 @@ int execute() /// funkce, ktera vezme instrukce z globalni tabulky prvku a vykon
             try_push_stack(DBOOL, retvalue);
             break;
         }
-        case ICMPL:
+        case ICMPL:{
+            univalue value1, value2, retvalue;
+            int dattype1, dattype2;
+            if (pop_stack(&dattype1, &value1) != 0) ExecError();
+            if (pop_stack(&dattype2, &value2) != 0) ExecError();
+            if (dattype1 != dattype2) {
+                if (dattype1 == DSTRING) free(value1.str);
+                if (dattype2 == DSTRING) free(value2.str);
+                //retvalue.log = SFALSE;
+                ExecError();
+            } else if (dattype1 == DNUM) {
+                if (value2.num  < value1.num) //PORADI!!
+                    retvalue.log = STRUE;
+                else
+                    retvalue.log = SFALSE;
+            } else if (dattype1 == DBOOL) {
+                ExecError();
+            } else if (dattype1 == DSTRING) {
+                if (strcmp(value2.str, value1.str) == -1)
+                    retvalue.log = STRUE;
+                else
+                    retvalue.log = SFALSE;
+                free(value1.str);
+                free(value2.str);
+            } else if (dattype1 == DNIL) ExecError();
+            try_push_stack(DBOOL, retvalue);
             break;
+        }
         case ICMPG:
             break;
         case ICMPEL:

52d6619e000e9830695e49d91849760bb6f3eb2e
alda519 <galda519@gmail.com>
2011-12-08 16:40:41 +0100

    Revert "Revert "#18 hotovo""
    This reverts commit 2e0dcc64bb5b7eba9adb6403c1072be651f00613.

--- src/parser.c
+++ src/parser.c
@@ -46,7 +46,6 @@ static int formal_parametr_seq(void);
 static int formal_parametr_seq_z(void);
 static int local_declaration_seq(void);
 static int local_declaration_z(void);
-static int literal(void);
 static int statement_seq(void);
 static int statement(void);
 static int assign_z(void);
@@ -246,7 +245,7 @@ static int local_declaration_seq(void)
         return 1;
 }
 
-// SDSz -> = LIT ;
+// SDSz -> = expression ;
 // SDSz -> ;
 static int local_declaration_z(void)
 {
@@ -261,8 +260,9 @@ static int local_declaration_z(void)
     // rovnitko
     else if(token == ASSIGN) {
         get_token();
-        // literal
-        try( literal() ); 
+        // vyraz
+        try( expression() ); 
+        generate(IPOPI, last_local);
         // strednik
         check_token(SEMICOLON, ERROR_SYN_X_SMCLN);
         get_token();
@@ -270,46 +270,6 @@ static int local_declaration_z(void)
     }
     else
         return (token < 0) ? token : ERROR_SYN_UX_TOKEN;
-}
-
-// LIT -> num
-// LIT -> str
-// LIT -> nil
-// LIT -> true
-// LIT -> false
-static int literal()
-{
-    Data data;
-    switch(token) {
-        case NUMBER:
-            data.type = T_NUMBER;
-            data.value.num = strtod(str.str, NULL);
-            break;
-        case STRING:
-            data.type = T_STRING;
-            data.value.str = str.str;
-            try( str_new(&str, STR_INIT_LEN) );
-            break;
-        case NIL:
-            data.type = T_NIL;
-            break;
-        case TRUE:
-            data.type = T_BOOLEAN;
-            data.value.log = 1;
-            break;
-        case FALSE:
-            data.type = T_BOOLEAN;
-            data.value.log = 0;
-            break;
-        default:
-            return (token < 0) ? token : ERROR_SYN_UX_TOKEN;
-    }
-    try( insert_literal(data) );
-    // inicializace lok. promenne
-    generate(IPUSH, last_literal);
-    generate(IPOPI, last_local);
-    get_token();
-    return 1;
 }
 
 // SPS -> S ; SPS
